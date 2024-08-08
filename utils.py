784e37b5838d330ffb53b882dfac562fd56503a2
Jason Chen <c04jason@icloud.com>
2024-08-07 23:07:15 -0400

    Updated hsr JSON structure and utilize python concurrent

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
-from PIL import Image
-from tqdm import tqdm
-from io import BytesIO
-import requests
 import os
 import json
+import requests
+from io import BytesIO
+from PIL import Image
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def link_genshin_art(name):
     return f'https://enka.network/ui/UI_Gacha_AvatarImg_{name}.png'
@@ -27,12 +28,18 @@ def update_genshin():
     progress_bar = tqdm(total=len(data), desc="Updating Genshin Impact characters", unit="character")
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    for id_str, name in data.items():
+    def process_character(id_str, name):
         genshin_icon_art(current_dir, name, id_str)
         genshin_splash_art(current_dir, name, id_str)
         genshin_wish_art(current_dir, name, id_str)
+        return id_str
 
-        progress_bar.update(1)
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = [executor.submit(process_character, id_str, name) for id_str, name in data.items()]
+        
+        for future in as_completed(futures):
+            future.result()
+            progress_bar.update(1)
 
     progress_bar.close()
     print(f"Finished updating {len(data)} Genshin Impact characters")
@@ -95,45 +102,51 @@ def update_starrail():
     progress_bar = tqdm(total=len(data), desc="Updating Star Rail characters", unit="character")
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    for id_str in data:
-        starrail_icon_art(current_dir, id_str)
-        starrail_splash_art(current_dir, id_str)
-        starrail_wish_art(current_dir, id_str)
+    def process_character(id_str, name):
+        starrail_icon_art(current_dir, name, id_str)
+        starrail_splash_art(current_dir, name, id_str)
+        starrail_wish_art(current_dir, name, id_str)
+        return id_str
 
-        progress_bar.update(1)
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = [executor.submit(process_character, id_str, name) for id_str, name in data.items()]
+
+        for future in as_completed(futures):
+            future.result()
+            progress_bar.update(1)
 
     progress_bar.close()
     print(f"Finished updating {len(data)} Star Rail characters")
 
-def starrail_icon_art(current_dir, character):
+def starrail_icon_art(current_dir, character, id):
     try:
-        response = requests.get(link_starrail_icon(character))
+        response = requests.get(link_starrail_icon(id))
         character_icon = Image.open(BytesIO(response.content))
 
         text_files_dir = os.path.join(current_dir, "starrail", "icon")
         os.makedirs(text_files_dir, exist_ok=True)
 
-        output_path = os.path.join(text_files_dir, f'{character}.png')
+        output_path = os.path.join(text_files_dir, f'{id}.png')
         character_icon.save(output_path)
     except Exception as e:
         print(f"Failed to create icon art for {character}: {str(e)}")
 
-def starrail_splash_art(current_dir, character):
+def starrail_splash_art(current_dir, character, id):
     try:
-        response = requests.get(link_starrail_art(character))
+        response = requests.get(link_starrail_art(id))
         character_art = Image.open(BytesIO(response.content))
 
         text_files_dir = os.path.join(current_dir, "starrail", "splash")
         os.makedirs(text_files_dir, exist_ok=True)
 
-        output_path = os.path.join(text_files_dir, f'{character}.png')
+        output_path = os.path.join(text_files_dir, f'{id}.png')
         character_art.save(output_path)
     except Exception as e:
         print(f"Failed to create splash art for {character}: {str(e)}")
 
-def starrail_wish_art(current_dir, character):
+def starrail_wish_art(current_dir, character, id):
     try:
-        response = requests.get(link_starrail_art(character))
+        response = requests.get(link_starrail_art(id))
         character_image = Image.open(BytesIO(response.content))
 
         new_width = character_image.height * 0.3125 # 5:16 ratio
@@ -148,7 +161,7 @@ def starrail_wish_art(current_dir, character):
         text_files_dir = os.path.join(current_dir, "starrail", "wish")
         os.makedirs(text_files_dir, exist_ok=True)
 
-        output_path = os.path.join(text_files_dir, f'{character}.png')
+        output_path = os.path.join(text_files_dir, f'{id}.png')
         cropped_image.save(output_path)
     except Exception as e:
         print(f"Failed to create wish art for {character}: {str(e)}")
