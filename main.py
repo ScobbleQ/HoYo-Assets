b0f953a43358ff6c4535cd65cb7cd3403aba84fd
Jason Chen <c04jason@icloud.com>
2024-08-29 16:22:23 -0400

    Genshin Impact v5.0
    - Kachina, Kinich, Mualani
    - Adjusted Gaming and Liuyun

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Last Update -------------------
-# Genshin Impact: v4.8          |
+# Genshin Impact: v5.0          |
 # Star Rail: v2.4               |
 
 import utils
 
-update_genshin = False
-update_starrail = True
+update_genshin = True
+update_starrail = False
 
 if update_genshin:
     utils.update_genshin()
